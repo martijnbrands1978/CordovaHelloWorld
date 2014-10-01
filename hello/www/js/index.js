6a679ab4e041859a1358244c3d4f7ddb8287e9e3
martijnbrands1978 <martijnbrands1978@gmail.com>
2014-10-02 00:30:00 +0200

    add window.location at startup

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -43,7 +43,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+        window.location = "http://www.nu.nl";
         console.log('Received Event: ' + id);
     }
 };
