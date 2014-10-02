41129eec893b35687a8642d1a24a622a182805e4
martijnbrands1978 <martijnbrands1978@gmail.com>
2014-10-02 12:21:06 +0200

    change config

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -43,7 +43,7 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-        window.location = "http://www.telegraaf.nl";
+        window.location = "http://google.com";
         appendDebug('nu.nl loaded');
     }
 };
