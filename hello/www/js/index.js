2e67dea552c44ff5350a0c2c7ac3ff11d090ad0b
martijnbrands1978 <martijnbrands1978@gmail.com>
2014-10-02 15:07:11 +0200

    change config

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -40,7 +40,7 @@ var app = {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-		window.location = "http://www.nu.nl";
+		window.location = "https://www.google.com";
     	appendDebug('google loaded');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
