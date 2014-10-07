10c2218b8f1c7e8845af029bb2c123ec58f651ae
martijnbrands1978 <martijnbrands1978@gmail.com>
2014-10-07 20:40:34 +0200

    clean up

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -37,9 +19,6 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
         loadRemoteContent();
         nfc.addNdefListener(onNfc, success, failure);
         nfc.addTagDiscoveredListener(
@@ -49,11 +28,6 @@ var app = {
                 },
                 failure
             );
-        //window.location = "http://192.168.178.13:52878/Home/TagInfo";
-        //window.location = "https://google.com";
-    	//appendDebug('google loaded');
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
     }
 };
 
@@ -75,59 +49,14 @@ function failure(reason) {
     alert("Failed to add NDEF listener");
 }
 
-
-var ref;
-function openExternalSite(url) {
-
-    //ref = window.open(url, '_blank', 'location=no');
-    //ref.addEventListener('exit', function () {
-        //scanBarcode();
-    //});
-    //ref.addEventListener('loadstart', function (event) {
-        //clearInterval(interval);
-        //if (event.url == 'http://192.168.178.13:52878/Home/ScanTag') {
-            //ref.close();
-            //scanBarcode();
-        //}
-    //});
-
-    //var interval;
-    //ref.addEventListener('loadstop', function (event) {
-        //interval = setInterval(function () {
-            //pushInfo();
-
-        //});
-    //});
-}
-
-function pushInfo() {
-    ref.executeScript({ code: "scanNewTag()" }, function (value) {
-        if (value == 'newscan') {
-            ref.close();
-            scanBarcode();
-        }
-    });
-}
-
 function loadRemoteContent() {
     $("#mainbody").load("http://192.168.178.13:52878/Home/RemoteContent");
 
 }
 
-function alarmPhone() {
-    alert('alarm');
-}
-
-
 function scanBarcode() {
     cordova.plugins.barcodeScanner.scan(
       function (result) {
-          //appendDebug("We got a barcode\n" +
-                //"Result: " + result.text + "\n" +
-                //"Format: " + result.format + "\n" +
-               // "Cancelled: " + result.cancelled);
-          //alert(result.text);
-          //setResult(result.text);
           scannSuccesCallBack(result.text);
       },
       function (error) {
