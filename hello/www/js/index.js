b5cd74aca31028e4b814ff73f65f314a147e3c3b
martijnbrands1978 <martijnbrands1978@gmail.com>
2014-10-07 10:32:50 +0200

    change nfc event

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -64,7 +64,7 @@ function appendDebug(message) {
 
 function onNfc(nfcEvent) {
     // display the tag as JSON
-    scannSuccesCallBack(JSON.stringify(nfcEvent.tag));
+    scannSuccesCallBack('ID: ' + nfcEvent.tag.id + "--------" + JSON.stringify(nfcEvent.tag));
     //alert(JSON.stringify("tag read"));
 }
 
