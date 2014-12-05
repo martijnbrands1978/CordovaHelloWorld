c04251f525c347ff7bb2c32943e1822863916d04
martijnbrands1978 <martijnbrands1978@gmail.com>
2014-12-05 21:16:40 +0100

    change remote ip

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -50,7 +50,7 @@ function failure(reason) {
 }
 
 function loadRemoteContent() {
-    $("#mainbody").load("http://192.168.178.13:52878/Home/RemoteContent");
+    $("#mainbody").load("http://192.168.178.12:52878/Home/RemoteContent");
 
 }
 
